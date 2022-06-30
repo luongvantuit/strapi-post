52a3bc7ab5285dacf31d2e94ae5c2ad364def838
Lương Văn Tú <luongvantuit@gmail.com>
2022-06-30 20:35:56 +0700

    done base query test strapi

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -7,7 +7,49 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    /**
+     * @type {}
+     */
+    const extensionService = strapi.plugin("graphql").service("extension");
+    extensionService.use({
+      resolversConfig: {
+        "Mutation.createPost": {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+              if (!args.data.user || args.data.user != context.state.user.id) {
+                throw "Error create new post! Check your user id!";
+              }
+              const res = await next(parent, args, context, info);
+              return res;
+            },
+          ],
+          auth: true,
+        },
+        "Mutation.createComment": {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+              if (!args.data.user || args.data.user != context.state.user.id) {
+                throw "Error create new post! Check your user id!";
+              }
+              if (!args.data.post) {
+                throw "Post is Empty!";
+              }
+              const entries = await strapi.entityService.findOne(
+                "api::post.post",
+                args.data.post
+              );
+              if (!entries) {
+                throw "Can't found post!";
+              }
+              const res = await next(parent, args, context, info);
+              return res;
+            },
+          ],
+        },
+      },
+    });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
