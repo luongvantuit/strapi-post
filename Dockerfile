6f03b37384f8ee83f5ebc2fcdce807d759b7dc3b
Lương Văn Tú <luongvantuit@gmail.com>
2022-06-30 15:47:20 +0700

    docker file config & env database postgres

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,11 @@
 FROM node:16.15.1
+
+WORKDIR /usr/app/strapi
+
+COPY . .
+
+RUN yarn install
+
+EXPOSE 1337
+
+CMD [ "yarn","start" ]
--- config/database.js
+++ config/database.js
@@ -1,10 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "dev"),
+      user: env("DATABASE_USERNAME", "admin"),
+      password: env("DATABASE_PASSWORD", "123456"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      ssl: env.bool("DATABASE_SSL_SELF", false)
+        ? {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+          }
+        : false,
     },
     useNullAsDefault: true,
   },
